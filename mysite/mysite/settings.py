c519ec03ce8310fa60841218eb29f4aad9bbf2dd
viniciustxc3 <viniciustxc@yahoo.com.br>
2022-12-07 14:34:48 -0300

    update ignored files

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^w-l3*nlwp^ku+ypuq1zspcsi^g9@8067n$#pu#ry&x5a(d(&a'
+SECRET_KEY = 'django-insecure-^w-l3*nlwp^ku+ypuq1zspcsi^g9@8067n$#pu#ry&x5a(d(&a' 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
