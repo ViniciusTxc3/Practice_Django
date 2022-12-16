9a5dfa325379e04ca7be119d7c2ec22dc6a2b638
viniciustxc3 <viniciustxc@yahoo.com.br>
2022-12-16 09:38:38 -0300

    first aplication

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 # Precisa apontar a raiz da URLconf para o módulo polls.urls (mysitte/urls.py)
 urlpatterns = [
-    path('', views.index, name='index'),
-]
+    path('', views.index, name='index'), #/polls/
+    path('<int:question_id>/', views.detail, name='detail'), #/polls/5
+    path('<int:question_id>/results/', views.results, name='results'), #/polls/5/result  
+    path('<int:question_id>/vote/', views.vote, name='vote'), #/polls/5/results/vote
+]    
